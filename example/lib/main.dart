9c27ff2eb754b4ffeddfe48b818c9061b165a084
Tim <hi@timcreated.it>
2022-06-24 12:12:36 +0200

    added localization possibility

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       home: const MyHomePage(title: 'Rotation Stage'),
+      localizationsDelegates: [],
     );
   }
 }
